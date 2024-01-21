0a22ce42d7aab2295a04320a0489f74e09f43600
Jose Carlos Aires <88852273+JC-Aires@users.noreply.github.com>
2024-01-21 12:35:45 -0300

    Add files via upload

--- hangmanv2.py
+++ hangmanv2.py
@@ -5,7 +5,7 @@ Author: JC-Aires
 """
 
 import random
-from words import words_list
+from words import words_list, stages, logo
 
 
 def get_valid_word(words_list):  # Pick a word without dash or space (- or ' ').
@@ -18,63 +18,6 @@ def get_valid_word(words_list):  # Pick a word without dash or space (- or ' ').
     return word.upper()  # Use uppercase for all comparisons
 
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
 global word
 chosen_word = get_valid_word(words_list)
 word_length = len(chosen_word)
@@ -82,6 +25,8 @@ end_of_game = False
 lives = 6
 
 # print(f'Pssst, the solution is {chosen_word}.')  # Testing code
+
+print(logo)
 
 display = []  # Create blanks for the word length
 for _ in range(word_length):
@@ -92,16 +37,20 @@ while not end_of_game:
 
     guess = input("Guess a letter: ").upper()
 
+    if guess in display:
+        print(f"You already tried the letter {guess}")
+
     for position in range(word_length):  # Check if guessed letter is in the word
         letter = chosen_word[position]
         if letter == guess:
             display[position] = letter
 
     if guess not in chosen_word:  # If guess is not a letter in the chosen_word, reduce 'lives' by 1.
+        print(f"The letter {guess} is not in the word! You lose a life!")
         lives -= 1
         if lives == 0:  # Ends game if all tries(lives) are gone
             end_of_game = True
-            print("You lose!")
+            print(f"You lose! The word was {chosen_word}")
 
     print(f"{' '.join(display)}")
 
